30557db8973b5d12da185160eb2d8e49e0e2dc6e
Thiago Fernandes Dias <thiago.fernandesdias235@gmail.com>
2024-08-10 22:02:16 -0300

    Initial commit

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,4 @@
+require("config.lazy")
+require("config.commands")
+require("config.etc")
+require("config.set")
---
+++ dot_config/nvim/lua/config/commands.lua
@@ -0,0 +1,32 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+vim.keymap.set('i', 'kj', '<ESC>')
+vim.keymap.set('n', '<leader>nt', function () require('nvim-tree.api').tree.toggle() end, {})
+
+vim.cmd([[cabbrev help tab help]])
+vim.cmd([[
+cnoreabbrev W! w!
+cnoreabbrev W1 w!
+cnoreabbrev w1 w!
+cnoreabbrev Q! q!
+cnoreabbrev Q1 q!
+cnoreabbrev q1 q!
+cnoreabbrev Qa! qa!
+cnoreabbrev Qall! qall!
+cnoreabbrev Wa wa
+cnoreabbrev Wq wq
+cnoreabbrev wQ wq
+cnoreabbrev WQ wq
+cnoreabbrev wq1 wq!
+cnoreabbrev Wq1 wq!
+cnoreabbrev wQ1 wq!
+cnoreabbrev WQ1 wq!
+cnoreabbrev W w
+cnoreabbrev Q q
+cnoreabbrev Qa qa
+cnoreabbrev Qall qall
+]])
