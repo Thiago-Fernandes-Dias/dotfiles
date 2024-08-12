34a202d68882d7b479d697f1fa94c7523de43d08
Thiago Fernandes Dias <thiago.fernandesdias235@gmail.com>
2024-08-12 00:13:24 -0300

    Changes

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,9 @@
 return {
-	{
-		"rebelot/kanagawa.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			-- load the colorscheme here
-			vim.cmd([[colorscheme kanagawa]])
-		end,
-	},
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function ()
+        vim.cmd.colorscheme "catppuccin-mocha"
+    end
 }
